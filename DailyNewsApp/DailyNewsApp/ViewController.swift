065bcc54777ec39fcd43e2bf35e39d76c1ee94c2
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2023-03-12 14:35:38 +0300

    Initial commit

---
+++ DailyNewsApp/DailyNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNewsApp
+//
+//  Created by Василий Вырвич on 12.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
