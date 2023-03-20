e68ddbb26f3ce31a4b072f4dcbc4ba8d3f6e52c7
Vasili13 <90265333+Vasili13@users.noreply.github.com>
2023-03-20 17:27:37 +0300

    HorizontalCollectionView
    add horizontalCollectionView with paging

--- DailyNewsApp/DailyNewsApp/Controllers/ViewController.swift
+++ DailyNewsApp/DailyNewsApp/Controllers/ViewController.swift
@@ -11,26 +11,38 @@ import SnapKit
 class ViewController: UIViewController {
     
     lazy var horizontalNewsCollectionView: UICollectionView = {
-        let layout = UICollectionViewLayout()
+        let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
         cv.dataSource = self
         cv.delegate = self
+        cv.backgroundColor = .systemGray
+        cv.isPagingEnabled = true
         return cv
     }()
+    
+    let cellId = "cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "text"
+        
         view.addSubview(horizontalNewsCollectionView)
+        
+        horizontalNewsCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         
         updateViewConstraints()
     }
-    
+
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
         horizontalNewsCollectionView.snp.makeConstraints { make in
-            make.width.height.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+            make.top.equalTo(100)
         }
     }
 }
@@ -41,7 +53,15 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    
+        let colors: [UIColor] = [.cyan,.brown,.clear,.lightText,.lightGray,.green,.magenta]
+        cell.backgroundColor = colors[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: view.frame.width, height: view.frame.height )
     }
 }
 
